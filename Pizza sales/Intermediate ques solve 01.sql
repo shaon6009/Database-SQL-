fde6d008d3c3b8d46d32967c14e00a4c931df0a7
Md Rakibul Hasan Shaon <shaonrakibul00@gmail.com>
2025-11-04 02:06:02 +0600

    Add files via upload

---
+++ Pizza sales/Intermediate ques solve 01.sql
@@ -0,0 +1,34 @@
+use pizzahut;
+
+
+-- Join the necessary tables to find the total quantity of each pizza category ordered.
+select pizza_types.category, sum(orders_details.quantity) as quantity
+from pizza_types join pizzas on pizza_types.pizza_type_id= pizzas.pizza_type_id
+join orders_details  on orders_details.pizza_id= pizzas.pizza_id group by pizza_types.category order by quantity desc;
+
+
+
+-- Determine the distribution of orders by hour of the day.
+select hour(order_time), count(order_id) from orders group by hour(order_time);
+
+
+
+-- Join relevant tables to find the category-wise distribution of pizzas.
+select category, count(name) from pizza_types group by category;
+
+
+
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+select avg(quantity) as order_AVG_quantity from
+(select orders.order_date, sum(orders_details.quantity) as quantity
+from orders join orders_details on orders.order_id= orders_details.order_id
+group by orders.order_date) as order_quantity ;
+
+
+
+-- Determine the top 3 most ordered pizza types based on revenue.
+select pizza_types.name ,
+sum(orders_details.quantity * pizzas.price) as revenue
+from pizza_types join pizzas on pizzas.pizza_type_id= pizza_types.pizza_type_id
+join orders_details on orders_details.pizza_id= pizzas.pizza_id
+group by pizza_types.name order by revenue desc limit 5;
