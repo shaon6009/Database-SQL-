fd08b4d15c800f0834d6a90a71db256ddc5d2398
Md Rakibul Hasan Shaon <shaonrakibul00@gmail.com>
2025-12-14 23:50:11 +0600

    Update Intermediate ques solve 01.sql

--- Pizza sales/Intermediate ques solve 01.sql
+++ Pizza sales/Intermediate ques solve 01.sql
@@ -1,6 +1,5 @@
 use pizzahut;
 
-
 -- Join the necessary tables to find the total quantity of each pizza category ordered.
 select pizza_types.category, sum(orders_details.quantity) as quantity
 from pizza_types join pizzas on pizza_types.pizza_type_id= pizzas.pizza_type_id
